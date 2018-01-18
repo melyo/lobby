5674fff6f37981b2fe19a9a32d0702a7387b185a
melyo <armel.acuna@yahoo.com>
2018-01-18 08:33:56 +0800

    routes

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,17 @@
+var lobbyController = require('./controllers/lobbyController')
+
 module.exports = (app) => {
 
-  // 
+  // prefix
+  var prefix = '/api/v1'
+
+  // events
+  app.get(prefix+'/lobbies', lobbyController.collection)
+  app.post(prefix+'/lobbies', lobbyController.store)
+  app.patch(prefix+'/lobbies/:id/join', lobbyController.join)
+  app.patch(prefix+'/lobbies/:id/exit', lobbyController.exit)
+  app.delete(prefix+'/lobbies/:id', lobbyController.delete)
+  app.get(prefix+'/lobbies/:id/messages', lobbyController.messages)
+  app.post(prefix+'/lobbies/:id/messages', lobbyController.storeMessage)
 
 }
